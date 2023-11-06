eb5dae57f893407cc1256f95e5731a382f77e063
KrmencikLukas <145004730+KrmencikLukas@users.noreply.github.com>
2023-11-06 17:24:56 +0100

    stavba json v readform pomoci funkci

--- code/pages/admin/action/readForm.php
+++ code/pages/admin/action/readForm.php
@@ -7,7 +7,6 @@
     $id = 1;
     
     $form = $DBlib->fetchDataWithCondition("form", "*", "id=:id",[":id"=>$id])[0];
-    var_dump($questions);
 
     /*
     {
@@ -41,51 +40,42 @@
     }
     */
 
-    function jsonQuestions(){
-        global $DBlib, $id;
-        $questions = $DBlib->fetchDataWithCondition("question", "*", "form_id=:id",[":id"=>$id]);
-        $questionArr = [];
-        foreach($questions as $key => $value){
-            $questionArr[] = [
-                [
-                    "heading"=> $value["heading"],
-                    "description"=> $value["description"],
-                    "type"=> 1,
-                    "media"=> [
-                        "/mujobrazek.png",
-                    ],
-                    "settings"=> [
-    
-                    ],
-                    "answers"=> [
-                        "text odpovedi",
-                    ]
-                ],
-            ];
+    function questionSettings($id){
+        global $DBlib;
+        $settings = $DBlib->fetchDataWithCondition("question_settings", "*", "question_id=:id",[":id"=>$id]);
+        $settingsArr = [];
+        foreach($settings as $value){
+            $settingsArr[$value["key"]] = $value["value"];
         }
-        return $questionArr;
+        return $settingsArr;
     }
 
     function formSettings(){
         global $DBlib, $id;
-        $questions = $DBlib->fetchDataWithCondition("form_settings", "*", "form_id=:id",[":id"=>$id]);
+        $settings = $DBlib->fetchDataWithCondition("form_settings", "*", "form_id=:id",[":id"=>$id]);
+        $settingsArr = [];
+        foreach($settings as $value){
+            $settingsArr[$value["key"]] = $value["value"];
+        }
+        return $settingsArr;
+    }
+
+    function questions(){
+        global $DBlib, $id;
+        $questions = $DBlib->fetchDataWithCondition("question", "*", "form_id=:id",[":id"=>$id]);
         $questionArr = [];
-        foreach($questions as $key => $value){
+        foreach($questions as $value){
             $questionArr[] = [
-                [
                     "heading"=> $value["heading"],
                     "description"=> $value["description"],
                     "type"=> 1,
                     "media"=> [
                         "/mujobrazek.png",
                     ],
-                    "settings"=> [
-    
-                    ],
+                    "settings"=> questionSettings($value["id"]),
                     "answers"=> [
                         "text odpovedi",
                     ]
-                ],
             ];
         }
         return $questionArr;
@@ -95,11 +85,8 @@
     $json = [
         "ID" => $id,
         "name" => $form["name"],
-        "settings" => [
-
-        ],
-        "questions" => jsonQuestions(),
-    ]
-
+        "settings" => formSettings(),
+        "questions" => questions(),
+    ];
 
 ?>
