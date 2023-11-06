ccf02399104063bb5f8ee4f3d6c691643e0a6b24
haasda22 <haasda22@sps-prosek.cz>
2023-11-06 10:20:50 +0100

    knihovny na register

---
+++ code/assets/lib/php/HashLibrary.php
@@ -0,0 +1,10 @@
+<?php
+    function Hash($argPS, $argEM){
+        $hash1 = hash("sha256", $argPS);
+        $hash1 .= strrev(hash("sha384", $argEM));
+        $hash2 = hash("sha512", $argPS);
+        $revhash2 = strrev($hash2);
+        $return = $hash1.$revhash2;
+        return $return;
+    }
+?>
