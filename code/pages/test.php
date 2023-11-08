bf41f30378dd0ca568189d5a693100bbc590b5f0
KrmencikLukas <145004730+KrmencikLukas@users.noreply.github.com>
2023-11-08 13:15:35 +0100

    Přidání ikon na header sidebar

--- code/pages/test.php
+++ code/pages/test.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/global/general.css">
 </head>
 <body>
-    
+    <?php include("../assets/global/headerSidebar.php") ?>
 </body>
 </html>
