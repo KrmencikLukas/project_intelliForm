4877a3c86441ed67ebd7d5bb793ea4e5d8f70e66
KrmencikLukas <145004730+KrmencikLukas@users.noreply.github.com>
2023-11-10 12:27:28 +0100

    Dokonceni headru a sidebaru

--- code/assets/global/headerSidebar.php
+++ code/assets/global/headerSidebar.php
@@ -1,23 +1,37 @@
+<?php
+    //Pred includem mužete nastavit proměnné $location a $PageSpecific
+    //Do $location jen prostý string s informací, kde se uživatel nacházi
+    //Do $PageSpecific se může dát html kód toho, co se má zobrazit v headru mezi logem a user ikonou
+?>
+
 <div id="HeaderAndSidebar">
     <div id="header">
         <div id="HeaderLeft">
             <div id="logo">
                 <span>Logo placeholder</span>
             </div>
-            <div id="LocationName">
-                <p>Location name</p>
-            </div>
+            <?php
+            if(isset($location)){
+                echo "<div id='LocationName'><p>";
+                echo $location;
+                echo "</p></div>";
+            }
+            ?>
         </div>
         <div id="PageSpecific">
             <?php
-                if (!empty($PageSpecific)) {
+                if (isset($PageSpecific)) {
                     echo $PageSpecific;
                 }
                 //sem si přes $PageSpecific můžete dát co chete, pokud chcete
+
+                $userIconPath = '/../img/icons/user.svg';
             ?>
         </div>
         <div id="HeaderRight">
-            <img class="UserIcon" src="<?php echo __DIR__ ?>" alt="">
+            <div class="userIconDiv">
+                <img class="UserIcon" src="<?php echo absolutePath($userIconPath) ?>" alt="">
+            </div>
         </div>
     </div>
     <div id="Sidebar">
@@ -45,3 +59,13 @@
         </div>
     </div>
 </div>
+
+<?php
+    function absolutePath($path){
+        $absolutePath = realpath(__DIR__ . $path);
+        $serverName = $_SERVER['HTTP_HOST'];
+        $path = 'http://' . $serverName . str_replace($_SERVER['DOCUMENT_ROOT'], '', $absolutePath);
+        return $path;
+    }
+
+?>
