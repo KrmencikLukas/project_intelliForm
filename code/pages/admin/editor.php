c2d7966dd20063b01a9eaf525d809f4587934706
KrmencikLukas <145004730+KrmencikLukas@users.noreply.github.com>
2023-11-11 09:57:49 +0100

    Vytvoření a nalinkování editor.php

---
+++ code/pages/admin/editor.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inteliform - Editor</title>
+    <link rel="stylesheet" href="../../assets/global/general.css">
+    <link rel="stylesheet" href="editor.css">
+</head>
+<body>
+    <?php include("../../assets/global/headerSidebar.php") ?>
+    <div id="content">
+
+    </div>
+</body>
+</html>
