0260fea00b7d4efee5ea3c961a8f687e7d74f8be
haasda22 <haasda22@sps-prosek.cz>
2023-11-13 14:52:31 +0100

    create answer

---
+++ code/pages/admin/action/createAnswear.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    include("../../../assets/lib/php/db.php");
+    include("../../../assets/lib/php/DBlibrary.php");
+    $DBlib = new DatabaseFunctions($db);
+
+    if(isset($_POST["questionId"])){
+        $QuestionID = $_POST["questionId"];
+
+        $arr = ["Id" => $QuestionID ];
+
+        $id = $DBlib->insertData("answer",$arr);
+
+        echo json_encode($id);
+    }else{
+        header("Location: ../../error.php");
+    }
+
+?>
--- code/pages/error.php
+++ code/pages/error.php
@@ -1,20 +1,3 @@
 Ty se pokoušíš hacknout tuhle stránku??????
 Ne?
 Tak se ti tam něco posralo.
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div style=" display:flex; flex-direction:column; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">
-        <h1>Email verification</h1>
-        <p>Please paste this code into the verification field to finish the sign up process.</p>
-        <p style="width: fit-content; background-color: black; color: white; font-size: 24px; align-self: center;">'.$vercode.'</p>
-        <p>If you didnt ask for this Email please contact our support</p>
-    </div>
-</body>
-</html>
