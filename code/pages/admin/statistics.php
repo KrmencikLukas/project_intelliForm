7cc4484151f785d319449a0a980ac6a2b699b730
KrmencikLukas <145004730+KrmencikLukas@users.noreply.github.com>
2023-11-29 13:21:19 +0100

    prace na webovkach

---
+++ code/pages/admin/statistics.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Statistics</title>
+    <link rel="stylesheet" href="../../assets/lib/css/pretty-checkbox/dist/pretty-checkbox.min.css">
+    <link rel="stylesheet" href="../../assets/global/general.css">
+    <link rel="stylesheet" href="css/statistics.css">
+    <script src="../../assets/lib/js/general.js"></script>
+    <script src="../../assets/lib/js/profileMenu.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="js/statistics.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+
+    <?php 
+        $location = "Statistics";
+        include("../../assets/global/headerSidebar.php") 
+    ?>
+
+    <div id="content">
+        <div class="container">
+            <div class="radio">
+                
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
